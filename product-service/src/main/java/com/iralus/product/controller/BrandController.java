95fc2417116093f43ea6c141184f52181329b4d1
Shanaka <shanakauwu@gmail.com>
2022-06-15 22:52:26 +0530

    15/06/2022-22.51PM

--- product-service/src/main/java/com/iralus/product/controller/BrandController.java
+++ product-service/src/main/java/com/iralus/product/controller/BrandController.java
@@ -1,4 +1,43 @@
 package com.iralus.product.controller;
 
+import com.iralus.product.entity.Brand;
+import com.iralus.product.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/brands")
 public class BrandController {
+
+    private final BrandService brandService;
+
+    @Autowired
+    public BrandController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+    @PostMapping("/")
+    public Brand addBrand(@RequestBody Brand brand){
+        return brandService.addBrand(brand);
+    }
+
+    @GetMapping("/")
+    public List<Brand> fetchBrands(){
+        return brandService.fetchBrands();
+    }
+    @GetMapping("/{brandId}")
+    public Brand fetchBrandById(@PathVariable Long brandId){
+        return brandService.fetchBrandById(brandId);
+    }
+    @DeleteMapping("/{brandId}")
+    public Brand deleteBrand(@PathVariable Long brandId){
+        return brandService.deleteBrand(brandId);
+    }
+    @PutMapping("/{brandId}")
+    public Brand updateBrand(@PathVariable Long brandId,
+                             @RequestBody Brand brand){
+        return brandService.updateBrand(brandId, brand);
+    }
 }
--- product-service/src/main/java/com/iralus/product/controller/ModelController.java
+++ product-service/src/main/java/com/iralus/product/controller/ModelController.java
@@ -1,4 +1,39 @@
 package com.iralus.product.controller;
 
+import com.iralus.product.entity.Model;
+import com.iralus.product.service.ModelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/models")
 public class ModelController {
+    private final ModelService modelService;
+    @Autowired
+    public ModelController(ModelService modelService) {
+        this.modelService = modelService;
+    }
+    @PostMapping("/")
+    public Model addModel(@RequestBody Model model){
+        return modelService.addBrand(model);
+    }
+    @GetMapping("/")
+    public List<Model> fetchModels(){
+        return modelService.fetchModels();
+    }
+    @GetMapping("/{modelId}")
+    public Model fetchModelById(@PathVariable Long modelId){
+        return modelService.fetchModelById(modelId);
+    }
+    @DeleteMapping("/{modelId}")
+    public Model deleteModel(@PathVariable Long modelId){
+        return modelService.deleteModel(modelId);
+    }
+    @PutMapping("/{modelId}")
+    public Model updateModel(@PathVariable Long modelId,
+                             @RequestBody Model model){
+        return modelService.updateModel(modelId, model);
+    }
 }
